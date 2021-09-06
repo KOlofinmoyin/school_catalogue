cc9b100fd6c94849d1486a5c5c2da0b8c08ea69e
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-09-06 10:13:57 +0100

    chore: add test examples, for running in node

--- main.js
+++ main.js
@@ -79,3 +79,35 @@ class HighSchool extends School{
           }
         }
 }
+
+// Primary School instance:
+console.log(`Task: PrimarySchool class instance, with #quickFacts() called on it:`);
+
+let lorraineHansbury = new PrimarySchool('Output: Lorraine Hansbury',514,'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+console.log('\n')
+
+console.log(`${lorraineHansbury.quickFacts()}`);
+
+console.log('\n---------------- o ---------------- \n')
+
+// //substitute teacher array
+substituteTeachers = ['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'];
+
+console.log(`
+Task: The principal of Lorraine Hansbury needs a substitute teacher for the day.
+
+Call .pickSubstituteTeacher() on School, and pass the following substitute teacher array as an argument:`)
+
+console.log('\n');
+
+console.log(`Output: Your substitute teacher is: ${School.pickSubstituteTeacher(substituteTeachers)}.`)
+
+
+// // High School instance:
+let alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(`The sports-teams at ${alSmith.name} are:`);
+alSmith.sportsTeams;
+
+console.log('\n---------------- o ---------------- \n')
